105d7ea0d2800aeb6b160c20042fbd0f3ede4f4e
jhdl0157 <jhdl0157@naver.com>
2022-09-23 13:03:51 +0900

    scppe add

--- src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/app_2022_09_22/job/helloWorld/HelloWorldJobConfig.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -15,10 +17,11 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
+
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job helloWorldJob(){
+    public Job helloWorldJob() {
         return jobBuilderFactory.get("helloWorldJob")
                 .incrementer(new RunIdIncrementer()) // 강제로 매번 다른 ID를 실행시에 파라미터로 부여
                 .start(helloWorldStep1())
@@ -26,18 +29,20 @@ public class HelloWorldJobConfig {
     }
 
     @Bean
-    public Step helloWorldStep1(){
+    @JobScope
+    public Step helloWorldStep1() {
         return stepBuilderFactory.get("helloWorldStep1")
                 .tasklet(helloWorldTasklet())
                 .build();
     }
 
     @Bean
-    public Tasklet helloWorldTasklet(){
+    @StepScope
+    public Tasklet helloWorldTasklet() {
         return (contribution, chunkContext) -> {
             System.out.println("헬로월드!");
+
             return RepeatStatus.FINISHED;
         };
     }
-
 }
